cc2be0c16d1e86ca178f430c81e78d8f978105a7
itsnorthCry3r <54860471+itsnorthCry3r@users.noreply.github.com>
2021-11-27 01:58:08 +0100

    add

---
+++ main.js
@@ -0,0 +1,28 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const r = urlParams.get('r')
+const i_ = urlParams.get('i')
+
+dat = data.imgs
+
+i = parseInt(i_)
+
+if (i !== null || i !== "null" || i !== "undefined" || i !== undefined) {
+    if (dat > i || dat === i) {
+        if (i !== 0) {
+            if (i > 0) {
+                console.log("yes")
+                window.location.href = "./img/" + i + ".jpg";
+            } else {
+                console.log("no")
+                window.location.href = "./404.jpg";
+            }
+        } else {
+            console.log("no")
+            window.location.href = "./404.jpg";
+        }
+    } else {
+        console.log("no")
+        window.location.href = "./404.jpg";
+    }
+}
